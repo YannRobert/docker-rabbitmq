b74b141133dea72a6dd81794b3b8397b7545b590
Yann ROBERT <yann.robert@anantaplex.fr>
2014-12-17 16:05:35 +0100

    added support to pass the SERVICE_HOST env variable to be used as the TLS Certificate CN

--- ssl.sh
+++ ssl.sh
@@ -3,13 +3,21 @@
 # set up ssl certificates
 
 if [[ ! -f /etc/rabbitmq/ssl/.ssl_config_generated ]]; then
+
+    # the SERVICE_HOST env variable should have been set by the docker 'run' command using the '-e' option
+    if test -z "${SERVICE_HOST}"
+    then
+        SERVICE_HOST=$(hostname)
+    fi
+    echo "SERVICE_HOST = ${SERVICE_HOST}"
+
     cd /ssl/testca
     openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
     cd ..
     mkdir server
     cd server
     openssl genrsa -out key.pem 2048
-    openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=$(hostname)/O=server/ -nodes
+    openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=${SERVICE_HOST}/O=server/ -nodes
     cd ../testca
     openssl ca -config openssl.cnf -in ../server/req.pem -out ../server/cert.pem -notext -batch -extensions server_ca_extensions
 
